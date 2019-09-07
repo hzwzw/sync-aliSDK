74e0137656eeae741eba39ad3ccb734642532d1a
hzwzw <hzwangzhiwei@gmail.com>
2019-09-07 15:08:45 +0800

    add lazada

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 var apiClient = require('./lib/api/topClient.js').TopClient;
 var tmcClient = require('./lib/tmc/tmcClient.js').TmcClient;
 var topBatch = require('./lib/api/topBatch.js').TopBatch;
+var checkSignForSpi = require('./lib/spiUtil.js').checkSignForSpi;
 
 module.exports = {
     ApiClient: apiClient,
     TmcClient: tmcClient,
     TopBatch: topBatch,
+    checkSignForSpi: checkSignForSpi,
 };
