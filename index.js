8678025b88376c0169f759c375705935d61417f4
hzwzw <hzwangzhiwei@gmail.com>
2019-09-28 10:55:14 +0800

    add error

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 var apiClient = require('./lib/api/topClient.js').TopClient;
 var tmcClient = require('./lib/tmc/tmcClient.js').TmcClient;
 var topBatch = require('./lib/api/topBatch.js').TopBatch;
-var checkSignForSpi = require('./lib/spiUtil.js').checkSignForSpi;
+var SpiUtil = require('./lib/spiUtil.js');
 
 module.exports = {
     ApiClient: apiClient,
     TmcClient: tmcClient,
     TopBatch: topBatch,
-    checkSignForSpi: checkSignForSpi,
+    SpiUtil: SpiUtil,
 };
