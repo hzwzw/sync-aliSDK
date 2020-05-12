c2dd03759e0393e9ff97268e925cad50f6f79c71
hzwzw <hzwangzhiwei@gmail.com>
2020-05-12 20:22:23 +0800

    ok

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 var apiClient = require('./lib/api/topClient.js').TopClient;
 var tmcClient = require('./lib/tmc/tmcClient.js').TmcClient;
 var topBatch = require('./lib/api/topBatch.js').TopBatch;
+var linkClient = require('./lib/api/linkClient').LinkClient;
+
 var SpiUtil = require('./lib/spiUtil.js');
 
 module.exports = {
     ApiClient: apiClient,
     TmcClient: tmcClient,
     TopBatch: topBatch,
+    LinkClient: linkClient,
     SpiUtil: SpiUtil,
 };
